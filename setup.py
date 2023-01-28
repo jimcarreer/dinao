905ce4bf6977adf0e8a1567ff5fb7a4c90984edf
Jim Carreer <jim.carreer@gmail.com>
2023-01-28 07:09:32 -0500

    Derp

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     test_suite="tests",
     install_requires=install_requires,
     tests_require=tests_require,
-    python_requires=">=3.8.0, <=3.11",
+    python_requires=">=3.8.0, <3.12",
     extras_require={"tests": tests_require},
     classifiers=[
         "Topic :: Database",
