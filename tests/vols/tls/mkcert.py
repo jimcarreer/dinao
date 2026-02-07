58e6428fd12c977a22588bf04aba7ac1810a3965
jim <jim.carreer@gmail.com>
2026-02-07 05:11:23 -0500

    Consolidate this stuff

--- tests/vols/tls/mkcert.py
+++ tests/vols/tls/mkcert.py
@@ -1,30 +1,29 @@
-###
-# Generates a server.crt / server.key to use on various backends for TLS/SSL modes
+"""Generates a server.crt / server.key to use on various backends for TLS/SSL modes."""
+
 import datetime
 
 # pip install cryptography
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend as dfb
-from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
-
 
 EXPIRE_DAYS = 36500
 PUBLIC_EXPONENT = 65537
 KEY_SIZE = 4096
 
 
-def load_ca() -> (rsa.RSAPrivateKey, x509.Certificate):
-    """Load the CA key and certificate pems for signing"""
-    with open("./ca.key", "rb") as fh:
+def load_ca() -> tuple[rsa.RSAPrivateKey, x509.Certificate]:
+    """Load the CA key and certificate pems for signing."""
+    with open("ca.key", "rb") as fh:
         key = serialization.load_pem_private_key(data=fh.read(), password=None, backend=dfb())
-    with open("./ca.crt", "rb") as fh:
+    with open("ca.crt", "rb") as fh:
         crt = x509.load_pem_x509_certificate(data=fh.read(), backend=dfb())
     return key, crt
 
 
 def mk_server_pair():
-    """Create server.key and server.crt signed by ca.key"""
+    """Create server.key and server.crt signed by ca.key."""
     ca_key, ca_crt = load_ca()
     now = datetime.datetime.now(tz=datetime.timezone.utc)
     exp = now + datetime.timedelta(days=EXPIRE_DAYS)
@@ -38,13 +37,15 @@ def mk_server_pair():
     crt = crt.not_valid_after(exp)
     crt = crt.not_valid_before(now)
     crt = crt.sign(private_key=ca_key, algorithm=hashes.SHA256(), backend=dfb())
-    with open("./server.key", "wb") as fh:
-        fh.write(key.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption()
-        ))
-    with open("./server.crt", "wb") as fh:
+    with open("server.key", "wb") as fh:
+        fh.write(
+            key.private_bytes(
+                encoding=serialization.Encoding.PEM,
+                format=serialization.PrivateFormat.TraditionalOpenSSL,
+                encryption_algorithm=serialization.NoEncryption(),
+            )
+        )
+    with open("server.crt", "wb") as fh:
         fh.write(crt.public_bytes(encoding=serialization.Encoding.PEM))
 
 
